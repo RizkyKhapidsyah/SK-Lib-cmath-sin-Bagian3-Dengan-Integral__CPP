729396913bccec631e29f4a0de19e7bce2c78e8d
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-05-22 22:10:57 +0700

    Add project files.

---
+++ SK-Lib-cmath-sin-Bagian3-Dengan-Integral__CPP/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by Programiz
+	Modified for Learn by Rizky Khapidsyah
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	int x = -1;
+	double result;
+
+	result = sin(x);
+	cout << "sin(x) = " << result << endl;
+
+	_getch();
+	return 0;
+}
